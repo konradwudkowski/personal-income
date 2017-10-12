43b6dd53df85d69943b1be873ff7cf608bd363d4
byronprentice <byron.prentice@digital.hmrc.gov.uk>
2017-10-12 11:43:00 +0100

    NGC-2473 Temporary revert of the previous payments implementation to hide current Android defect.

--- app/uk/gov/hmrc/personalincome/domain/userdata/Payments.scala
+++ app/uk/gov/hmrc/personalincome/domain/userdata/Payments.scala
@@ -35,11 +35,6 @@ case class PaymentSummary(workingTaxCredit: Option[PaymentSection], childTaxCred
       ++childTaxCredit.map(_.paymentSeq).getOrElse(Seq.empty))
   }
 
-  def previousTotalsByDate: Option[List[Total]] = {
-    total(workingTaxCredit.flatMap(_.previousPaymentSeq).getOrElse(Seq.empty)
-        ++childTaxCredit.flatMap(_.previousPaymentSeq).getOrElse(Seq.empty))
-  }
-
   private def total(payments: Seq[Payment]): Option[List[Total]] = {
     if(payments.isEmpty) None
     else {
@@ -50,8 +45,7 @@ case class PaymentSummary(workingTaxCredit: Option[PaymentSection], childTaxCred
   }
 }
 
-case class PaymentSection(paymentSeq: List[Payment], paymentFrequency: String,
-                          previousPaymentSeq: Option[List[Payment]] = None)
+case class PaymentSection(paymentSeq: List[Payment], paymentFrequency: String)
 
 case class Payment(amount: BigDecimal, paymentDate: DateTime, oneOffPayment: Boolean)
 
@@ -89,11 +83,10 @@ object PaymentSummary {
         (__ \ "paymentEnabled").write[Boolean] ~
         (__ \ "specialCircumstances").writeNullable[String] ~
         (__ \ "informationMessage").writeNullable[String] ~
-        (__ \ "totalsByDate").writeNullable[List[Total]] ~
-        (__ \ "previousTotalsByDate").writeNullable[List[Total]]
+        (__ \ "totalsByDate").writeNullable[List[Total]]
       ).tupled
 
-      paymentSummaryWrites.writes(paymentSummary.workingTaxCredit, paymentSummary.childTaxCredit, paymentSummary.paymentEnabled, paymentSummary.specialCircumstances, paymentSummary.informationMessage, paymentSummary.totalsByDate, paymentSummary.previousTotalsByDate)
+      paymentSummaryWrites.writes(paymentSummary.workingTaxCredit, paymentSummary.childTaxCredit, paymentSummary.paymentEnabled, paymentSummary.specialCircumstances, paymentSummary.informationMessage, paymentSummary.totalsByDate)
     }
   }
 
