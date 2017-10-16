41546a6e59be6671d52ae68d465dfa7859b9cfe4
byronprentice <byron.prentice@digital.hmrc.gov.uk>
2017-10-16 11:03:48 +0100

    NGC-2477 -> Revert "NGC-2473 Temporary revert of the previous payments implementation to hide current Android defect."
    This reverts commit 43b6dd5

--- test/uk/gov/hmrc/personalincome/domain/PaymentSummarySpec.scala
+++ test/uk/gov/hmrc/personalincome/domain/PaymentSummarySpec.scala
@@ -183,6 +183,216 @@ class PaymentSummarySpec extends UnitSpec with WithFakeApplication {
 
       Json.stringify(Json.toJson(paymentSummary)) shouldBe expectedResponse
     }
+    "correctly parse the previous payments and associated totals for wtc" in {
+
+      val wtc =
+        s"""
+           |"workingTaxCredit": {
+           |  "paymentSeq": [
+           |    ${payment(55.00, date(1, now.plusMonths), false)},
+           |    ${payment(55.00, date(2, now.plusMonths), false)},
+           |    ${payment(55.00, date(3, now.plusMonths), false)}
+           |  ],
+           |  "paymentFrequency": "weekly",
+           |  "previousPaymentSeq": [
+           |    ${payment(33.00, date(1, now.minusMonths), false)},
+           |    ${payment(43.00, date(2, now.minusMonths), false)},
+           |    ${payment(53.00, date(3, now.minusMonths), false)}
+           |  ]
+           |}
+         """.stripMargin
+
+      val ctc =
+        s"""
+           |"childTaxCredit": {
+           |  "paymentSeq": [
+           |    ${payment(55.00, date(1, now.plusMonths), false)},
+           |    ${payment(55.00, date(2, now.plusMonths), false)},
+           |    ${payment(55.00, date(3, now.plusMonths), false)}
+           |  ],
+           |  "paymentFrequency": "weekly"
+           |}
+         """.stripMargin
+
+      val totalsByDate =
+        s"""
+           |"totalsByDate": [
+           |  ${total(110.00, date(1, now.plusMonths))},
+           |  ${total(110.00, date(2, now.plusMonths))},
+           |  ${total(110.00, date(3, now.plusMonths))}
+           |]
+         """.stripMargin
+
+      val previousTotalsByDate =
+        s"""
+           |"previousTotalsByDate": [
+           | ${total(53.00, date(3, now.minusMonths))},
+           | ${total(43.00, date(2, now.minusMonths))},
+           | ${total(33.00, date(1, now.minusMonths))}
+           |]
+         """.stripMargin
+
+      val request = s"""{ $wtc, $ctc, "paymentEnabled": true}""".stripMargin
+      val expectedResponse = Json.stringify(Json.parse(s"""{ $wtc, $ctc, "paymentEnabled": true, $totalsByDate, $previousTotalsByDate }"""))
+      val response = Json.parse(request).validate[PaymentSummary]
+      val paymentSummary = response match {
+        case success: JsSuccess[PaymentSummary] => {
+          success.get
+        }
+      }
+      paymentSummary.paymentEnabled shouldBe true
+      paymentSummary.childTaxCredit.isDefined shouldBe true
+      paymentSummary.workingTaxCredit.isDefined shouldBe true
+      paymentSummary.totalsByDate.isDefined shouldBe true
+      paymentSummary.previousTotalsByDate.isDefined shouldBe true
+      Json.stringify(Json.toJson(paymentSummary)) shouldBe expectedResponse
+    }
+    "correctly parse the previous payments and associated totals for ctc" in {
+
+      val wtc =
+        s"""
+           |"workingTaxCredit": {
+           |  "paymentSeq": [
+           |    ${payment(55.00, date(1, now.plusMonths), false)},
+           |    ${payment(55.00, date(2, now.plusMonths), false)},
+           |    ${payment(55.00, date(3, now.plusMonths), false)}
+           |  ],
+           |  "paymentFrequency": "weekly"
+           |}
+         """.stripMargin
+
+      val ctc =
+        s"""
+           |"childTaxCredit": {
+           |  "paymentSeq": [
+           |    ${payment(55.00, date(1, now.plusMonths), false)},
+           |    ${payment(55.00, date(2, now.plusMonths), false)},
+           |    ${payment(55.00, date(3, now.plusMonths), false)}
+           |  ],
+           |  "paymentFrequency": "weekly",
+           |  "previousPaymentSeq": [
+           |    ${payment(33.00, date(1, now.minusMonths), false)},
+           |    ${payment(43.00, date(2, now.minusMonths), false)},
+           |    ${payment(53.00, date(3, now.minusMonths), false)}
+           |  ]
+           |}
+         """.stripMargin
+
+      val totalsByDate =
+        s"""
+           |"totalsByDate": [
+           |  ${total(110.00, date(1, now.plusMonths))},
+           |  ${total(110.00, date(2, now.plusMonths))},
+           |  ${total(110.00, date(3, now.plusMonths))}
+           |]
+         """.stripMargin
+
+      val previousTotalsByDate =
+        s"""
+           |"previousTotalsByDate": [
+           | ${total(53.00, date(3, now.minusMonths))},
+           | ${total(43.00, date(2, now.minusMonths))},
+           | ${total(33.00, date(1, now.minusMonths))}
+           |]
+         """.stripMargin
+
+      val request = s"""{ $wtc, $ctc, "paymentEnabled": true}""".stripMargin
+      val expectedResponse = Json.stringify(Json.parse(s"""{ $wtc, $ctc, "paymentEnabled": true, $totalsByDate, $previousTotalsByDate }"""))
+      val response = Json.parse(request).validate[PaymentSummary]
+      val paymentSummary = response match {
+        case success: JsSuccess[PaymentSummary] => {
+          success.get
+        }
+      }
+      paymentSummary.paymentEnabled shouldBe true
+      paymentSummary.childTaxCredit.isDefined shouldBe true
+      paymentSummary.workingTaxCredit.isDefined shouldBe true
+      paymentSummary.totalsByDate.isDefined shouldBe true
+      paymentSummary.previousTotalsByDate.isDefined shouldBe true
+      Json.stringify(Json.toJson(paymentSummary)) shouldBe expectedResponse
+    }
+    "totals are calculated correctly for wtc and ctc with future and previous payments" in {
+      val wtc =
+        s"""
+           |"workingTaxCredit": {
+           |  "paymentSeq": [
+           |    ${payment(21.33, date(1, now.plusMonths), false)},
+           |    ${payment(33.33, date(2, now.plusMonths), false)},
+           |    ${payment(33.33, date(2, now.plusMonths), false)},
+           |    ${payment(22.95, date(2, now.plusMonths), false)},
+           |    ${payment(89.61, date(3, now.plusMonths), false)}
+           |  ],
+           |  "paymentFrequency": "weekly",
+           |  "previousPaymentSeq": [
+           |    ${payment(33.12, date(2, now.minusMonths), false)},
+           |    ${payment(33.56, date(2, now.minusMonths), false)},
+           |    ${payment(53.65, date(5, now.minusMonths), false)},
+           |    ${payment(50.35, date(5, now.minusMonths), false)},
+           |    ${payment(53.00, date(5, now.minusMonths), false)}
+           |  ]
+           |}
+         """.stripMargin
+
+      val ctc =
+        s"""
+           |"childTaxCredit": {
+           |  "paymentSeq": [
+           |    ${payment(105.88, date(1, now.plusMonths), false)},
+           |    ${payment(100.55, date(2, now.plusMonths), false)},
+           |    ${payment(5.33, date(2, now.plusMonths), false)},
+           |    ${payment(100.55, date(3, now.plusMonths), false)},
+           |    ${payment(2.66, date(3, now.plusMonths), false)},
+           |    ${payment(2.67, date(3, now.plusMonths), false)}
+           |  ],
+           |  "paymentFrequency": "weekly",
+           |  "previousPaymentSeq": [
+           |    ${payment(333.33, date(1, now.minusMonths), false)},
+           |    ${payment(333.33, date(1, now.minusMonths), false)},
+           |    ${payment(333.33, date(1, now.minusMonths), false)},
+           |    ${payment(213.00, date(2, now.minusMonths), false)},
+           |    ${payment(213.00, date(2, now.minusMonths), false)},
+           |    ${payment(213.00, date(2, now.minusMonths), false)},
+           |    ${payment(360.99, date(2, now.minusMonths), false)},
+           |    ${payment(153.12, date(3, now.minusMonths), false)},
+           |    ${payment(846.87, date(3, now.minusMonths), false)}
+           |  ]
+           |}
+         """.stripMargin
+
+      val totalsByDate =
+        s"""
+           |"totalsByDate": [
+           |  ${total(127.21, date(1, now.plusMonths))},
+           |  ${total(195.49, date(2, now.plusMonths))},
+           |  ${total(195.49, date(3, now.plusMonths))}
+           |]
+         """.stripMargin
+
+      val previousTotalsByDate =
+        s"""
+           |"previousTotalsByDate": [
+           | ${total(157.00, date(5, now.minusMonths))},
+           | ${total(999.99, date(3, now.minusMonths))},
+           | ${total(1066.67, date(2, now.minusMonths))},
+           | ${total(999.99, date(1, now.minusMonths))}
+           |]
+         """.stripMargin
+
+      val request = s"""{ $wtc, $ctc, "paymentEnabled": true}""".stripMargin
+      val expectedResponse = Json.stringify(Json.parse(s"""{ $wtc, $ctc, "paymentEnabled": true, $totalsByDate, $previousTotalsByDate }"""))
+      val response = Json.parse(request).validate[PaymentSummary]
+      val paymentSummary = response match {
+        case success: JsSuccess[PaymentSummary] => {
+          success.get
+        }
+      }
+      paymentSummary.paymentEnabled shouldBe true
+      paymentSummary.childTaxCredit.isDefined shouldBe true
+      paymentSummary.workingTaxCredit.isDefined shouldBe true
+      paymentSummary.totalsByDate.isDefined shouldBe true
+      paymentSummary.previousTotalsByDate.isDefined shouldBe true
+      Json.stringify(Json.toJson(paymentSummary)) shouldBe expectedResponse
+    }
   }
   "correctly parse the informationMessage" in {
     val wtc =
